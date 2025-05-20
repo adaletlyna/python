599a0e00f7362a35bcb6be000323b7fbc581870c
lyna adalet <adaletlyna27@gmail.com>
2025-05-20 15:56:07 +0100

    Chaining Methods

--- User.py
+++ User.py
@@ -12,26 +12,23 @@ class User:
         print (self.__dict__)
         for key, value in self.__dict__.items():     #self.items does not work cause .items() only works with dictionaries not class instance (self)
             print(f"{key}: {value}")    #__dict__: special built-in attribute =>stores all instance attributes in a dictionary
-
+        return self
     def enroll(self):
         if self.is_rewards_member:
             print("User already a member.")
-            return False
+            return self
         self.is_rewards_member=True
         self.gold_card_points=200
-        return True
+        return self
 
     def spend_points(self, amount):
         if amount> self.gold_card_points:
             print(f"Not enough points to spend. Available: {self.gold_card_points}, Tried to spend: {amount}")
-            return False
+            
         else:
             self.gold_card_points -=amount
             print("available: ", self.gold_card_points)
-            return True
+        return self
         
 user1 = User("lyna","adalet","lyna@jjj.com",21)
-user1.display_info()
-print(user1.enroll())
-user1.spend_points(100)
-print(user1.enroll())
+user1.display_info().enroll().spend_points(100) # each method should return self to enable the chaining(it is like self.method)
